2f005ea6be091bbd10ba7f93ddf68cb5c1fbd3a1
rebecca3miller <rebecca.miller@keyin.com>
2026-08-14 13:56:06 -0230

    Created getters and setters.

--- Membership.java
+++ Membership.java
@@ -15,5 +15,45 @@ public class Membership {
         this.price = price;
     }
 
-    
+    public void setMembershipId(int membershipId) {
+        this.membershipId = membershipId;
+    }
+
+    public int getMembershipId() {
+        return this.membershipId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getUserId() {
+        return this.userId;
+    }
+
+    public void setMembershipType(String membershipType) {
+        this.membershipType = membershipType;
+    }
+
+    public String getMembershipType() {
+        return this.membershipType;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }   
+
+    @Override
+    public String toString() {
+        return "Membership{" +
+                "membershipId=" + membershipId +
+                ", userId=" + userId +
+                ", membershipType='" + membershipType + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
