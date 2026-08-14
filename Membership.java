d8171ec5fcbfbe4da7db4fb14c923d11079cb89d
rebecca3miller <rebecca.miller@keyin.com>
2026-08-14 13:53:37 -0230

    Set up public membership constructor.

--- Membership.java
+++ Membership.java
@@ -1,3 +1,19 @@
-public class membership {
+public class Membership {
+    private int membershipId;
+    private int userId;
+    private String membershipType;
+    private double price;
+
+    public Membership() {
+
+    }
+
+    public Membership(int membershipId, int userId, String membershipType, double price) {
+        this.membershipId = membershipId;
+        this.userId = userId;
+        this.membershipType = membershipType;
+        this.price = price;
+    }
+
     
 }
